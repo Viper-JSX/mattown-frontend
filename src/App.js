bec04edab3fb6e8c70bb0c577722d7f36f49eea0
Yurii Shtefanko <yuramcpecs@gmail.com>
2023-12-05 20:40:59 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./css/main.scss"
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App(){
+    return(
+        <div className='App'>222</div>
+    );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"), //Files output folder
+        filename: "bundle.js" //Bundled files. The file where all files are bundled to
+    },
+
+    module:{
+        rules: [
+            {
+                test: /\.(js|jsx)$/, //If encounters .js or .jsx file then use babel-loader to handle the file
+                exclude: /node_modules/, //We don't need to transpile node_modules
+                use:{
+                    loader:  "babel-loader"
+                }
+            },
+
+            {
+                test: /\.(s*)css$/, //Same for .css files
+                use: [ "style-loader", "css-loader", "sass-loader"] //these are ecexuted in order from the end: css-loader ->  style-loader -> output, or sass-loader -> css-loader ->  style-loader -> output
+                
+            }
+        ]
+    },
+
+    plugins: [ new HtmlWebPackPlugin({ template: "./src/index.html" })], //This handles the html template
+}
