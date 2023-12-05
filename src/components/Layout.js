3bc4b723a8ad35166871a8749a13d33e2ff2d45d
Yurii Shtefanko <yuramcpecs@gmail.com>
2023-12-05 21:01:43 +0200

    Added profile route

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,6 +6,8 @@ const Layout = () => {
             <Route path="" element={ <h2>Feed</h2> } />
             <Route path="/auth" element={ <h2>Auth</h2> } />
 
+            <Route path="/profile" element={ <h2>User Profile</h2> } />
+
             <Route path="/communities" element={ <h2>Communities</h2> }>
                 <Route path="" element={ <h2>Communities list</h2> } />
                 <Route path=":id" element={ <h2>Community</h2> } />
