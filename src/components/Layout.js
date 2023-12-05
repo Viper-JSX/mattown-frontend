6fe138ad72fcaea23e172e3f0f41736ce84bd05f
Yurii Shtefanko <yuramcpecs@gmail.com>
2023-12-05 20:58:08 +0200

    Added routes for communities

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,8 +3,13 @@ import { Routes, Route } from "react-router-dom";
 const Layout = () => {
     return (
         <Routes>
-            <Route path="" element={ <h2>Home page</h2> } />
+            <Route path="" element={ <h2>Feed</h2> } />
             <Route path="/auth" element={ <h2>Auth</h2> } />
+
+            <Route path="/communities" element={ <h2>Communities</h2> }>
+                <Route path="" element={ <h2>Communities list</h2> } />
+                <Route path=":id" element={ <h2>Community</h2> } />
+            </Route>
         </Routes>
     );
 }
