a67b84275b22b5ee54f016d29939a92843b0e782
Yurii Shtefanko <yuramcpecs@gmail.com>
2023-12-05 21:17:26 +0200

    Created root reducer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { createRoot } from "react-dom/client";
-import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+
+import store from "./redux/store";
+
+import App from "./App";
+
  
 
 
@@ -10,7 +15,9 @@ const rootElement = document.getElementById("root")
 const root = createRoot(rootElement);
 root.render(
     <BrowserRouter>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </BrowserRouter>
 );
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
- 
-const store = configureStore({}, {});
+
+import rootReducer from "./reducers/rootReducer";
+
+const store = configureStore({ reducer: rootReducer });
 
 export default store;
