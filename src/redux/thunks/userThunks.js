26e4de6b7c097fbd32fef3a34f41932b05e4a25b
Yurii Shtefanko <yuramcpecs@gmail.com>
2023-12-07 20:34:52 +0200

    Creted thunks: signIn and signUp

---
+++ src/redux/thunks/userThunks.js
@@ -0,0 +1,12 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+const signIn = createAsyncThunk("/user/sign-in", async (state, action) => {
+
+});
+
+
+const signUp = createAsyncThunk("/user/sign-in", async (state, action) => {
+
+});
+
+export { signIn, signUp }
