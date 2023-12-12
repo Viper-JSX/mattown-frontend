323011f1cc7fb4b39886c2e7f47ec291bd479afa
Yurii Shtefanko <yuramcpecs@gmail.com>
2023-12-12 15:20:47 +0200

    Created axios client and replaced axios object with axiosClient

---
+++ src/axios/client.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+const axiosClient = axios.create({ baseURL: "http://localhost:1337/api/" });
+
+export default axiosClient;
--- src/redux/thunks/userThunks.js
+++ src/redux/thunks/userThunks.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import axiosClient from "../../axios/client";
 
 const signIn = createAsyncThunk("/user/sign-in", async (state, action) => {
     //const response = await fetch("http://localhost:1337/api/auth/sign-in", { method: "post", body: JSON.stringify({ email: "sss", password: 1})});
-    const response = await axios.post("http://localhost:1337/api/auth/sign-in", { email: "yura@gmail.com", password: "yurashtefanko"});
+    const response = await axiosClient.post("/auth/sign-in", { email: "yura@gmail.com", password: "yurashtefanko"});
     console.log(response.data);
     return response.data; //response is an object with data and info about request, json() method returns the data you received from server as body
 });
