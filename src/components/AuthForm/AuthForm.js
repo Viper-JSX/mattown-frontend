657040e83173628191417b4b62e6b16955c124bf
Yurii Shtefanko <yuramcpecs@gmail.com>
2023-12-14 17:08:37 +0200

    Changed name of input

--- src/components/AuthForm/AuthForm.js
+++ src/components/AuthForm/AuthForm.js
@@ -7,10 +7,12 @@ import SignUp from "./SignUp.js";
 
 
 const AuthForm = ({ handleSignIn, handleSignUp }) => {
-  const [ mode, setMode ] = useState(authFormModes.signIn);
+  const [ mode, setMode ] = useState(authFormModes.signUp);
   const [ values, setValues ] = useState({email: "", password: ""});
   
     function handleSubmit (event) {
+        event.preventDefault();
+
         if (mode === authFormModes.signIn) {
             return handleSignIn(event);
         }
