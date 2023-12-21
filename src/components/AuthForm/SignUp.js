272b49c95bb0eec35a6dbce7be6b065daa5a6f2e
Yurii Shtefanko <yuramcpecs@gmail.com>
2023-12-21 06:17:06 +0200

    Added inputs for firstname and lastname to SignUp component

--- src/components/AuthForm/SignUp.js
+++ src/components/AuthForm/SignUp.js
@@ -1,8 +1,10 @@
 const SignUp = ({ values, handleChange }) => {
     return (
         <div>
-            <input type="email" name="email" value={values.email} onChange = {handleChange}/>
-            <input type="password" name="password" value={values.password} onChange = {handleChange}/>
+            <input type="text" name="firstname" value={values.firstname} onChange={handleChange}/>
+            <input type="text" name="lastname" value={values.lastname} onChange={handleChange}/>
+            <input type="email" name="email" value={values.email} onChange={handleChange}/>
+            <input type="password" name="password" value={values.password} onChange={handleChange}/>
         </div>
     )
 }
