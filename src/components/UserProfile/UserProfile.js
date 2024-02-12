2bb519a61a292918b568cfa8820fb2fde4df506a
Yurii Shtefanko <yuramcpecs@gmail.com>
2024-02-12 21:45:58 +0200

    Accessed user id from searchParams and implemented profile view mode identification

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -1,4 +1,9 @@
+import { useParams } from "react-router-dom";
+
 const UserProfile = () => {
+    const { id } = useParams(); //User ID
+    const profileViewMode = Boolean(id); //If there is an id it means that user views other user's profile, otherwise user views their own profile
+
     return (
         <div className="user-profile">
             User
